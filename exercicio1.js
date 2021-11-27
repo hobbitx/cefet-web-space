8a7b1a14b26ba0884a43036ffb4cba42ca648442
Robert Souza <robert.cristian@live.com>
2021-11-27 10:25:36 -0300

    function to calculate gravitation

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,14 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let btnCalcular = document.querySelector('#calcular');
+
+btnCalcular.addEventListener('click', function() {
+    let massa1 = document.querySelector('#massa1');
+    let massa2 = document.querySelector('#massa2');
+    let distancia = document.querySelector('#distancia');
+    let constante = document.querySelector('#constante');
+    let resultado = document.querySelector('#resultado');
+
+    resultado.value = ((constante.value*massa1.value*massa2.value)/(distancia.value**2)).toPrecision(4);
+}); 
